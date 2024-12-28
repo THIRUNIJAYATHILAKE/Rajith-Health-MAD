4fb5963edffabab2db23b86e3205c4de59486884
THIRUNIJAYATHILAKE <thiruninavodya18@gmail.com>
2024-12-28 19:49:04 +0530

    updated

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './components/LoginScreen';
 import { RootStackParamList } from './navigation/types';
 import RegisterScreen from './components/RegistrationScreen';
+import GetStartedScreen from './components/GetStartedScreen';
 import HomeScreen from './components/HomeScreen';
 import { CountProvider } from './context/CountContext';
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -11,11 +12,12 @@ const Index: React.FC = () => {
   return (
     <CountProvider>
     <Stack.Navigator
-      initialRouteName="Login"
+      initialRouteName="GetStarted"
       screenOptions={{
         headerShown: false,
       }}
     >
+      <Stack.Screen name="GetStarted" component={GetStartedScreen}  />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
